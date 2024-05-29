1b8822ee74acc2d2e00513cc2dbac04234c7a432
Tshentro tech <131613374+TshentroTech@users.noreply.github.com>
2024-05-29 20:09:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculateAge() {
+    const birthdate = document.getElementById('birthdate').value;
+    const birthDate = new Date(birthdate);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    document.getElementById('result').innerText = `You are ${age} years old.`;
+}
